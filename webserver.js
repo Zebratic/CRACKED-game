b8363d559efa37a393c94eaa004123e6a414b93b
Zebratic <zebraticyt@gmail.com>
2024-05-13 09:00:16 +0200

    removed ejs

--- webserver.js
+++ webserver.js
@@ -1,21 +1,9 @@
-// file deepcode ignore PT: <please specify a reason of ignoring this>
-// file deepcode ignore NoRateLimitingForExpensiveWebOperation: <please specify a reason of ignoring this>
 const express = require('express');
 
 function setupWebserver(app) {
-    app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
 
     app.use(express.static('views'));
-
-    // fix path traversal vulnerability
-    app.use((req, res, next) => {
-        if (req.path.includes('..')) {
-            res.status(403).send('Forbidden');
-            return;
-        }
-        next();
-    });
 
     app.get('/', (req, res) => {
         res.render('index', { title: 'Main Menu' });
@@ -36,8 +24,8 @@ function setupWebserver(app) {
     console.log('Webserver setup complete');
 }
 
-
-
 module.exports = {
     setupWebserver,
 };
+
+
