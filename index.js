7a4806e895c3f4ede761d54fb1be2114c48ca73a
Zebratic <zebraticyt@gmail.com>
2024-05-13 08:35:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 
-const webserver = require('./webserver');
+const webserver = require('./webserver.js');
 webserver.setupWebserver(app);
 
 app.listen(process.env.PORT, () => {
